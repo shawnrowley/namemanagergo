f4c843b25890bd6a0e24f323213f0acc0d93eddd
Shawn Rowley <srowley@fedora22.workstation.home>
2016-07-27 23:18:57 -0400

    added updated

--- controllers/person.go
+++ controllers/person.go
@@ -1,129 +1,162 @@
 package controllers
 
 import (
-		"encoding/json"
-		"fmt"
-		"net/http"
-  	"github.com/julienschmidt/httprouter"
-		"github.com/shawnrowley/namemanagergo/models"
-		"gopkg.in/mgo.v2"
-		"gopkg.in/mgo.v2/bson"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/shawnrowley/namemanagergo/models"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type (
-		PersonController struct {
+	// PersonController struct
+	PersonController struct {
 		session *mgo.Session
 	}
 )
 
+// NewPersonController creates
 func NewPersonController(s *mgo.Session) *PersonController {
 	return &PersonController{s}
 }
 
 func getURLJson(url string, target interface{}) error {
-    r, err := http.Get(url)
-    if err != nil {
-        return err
-    }
-    defer r.Body.Close()
-    return json.NewDecoder(r.Body).Decode(target)
+	r, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+	return json.NewDecoder(r.Body).Decode(target)
 }
 
-// GetGenderReport
+// GetGenderReport get Person Report
 func (pc PersonController) GetGenderReport(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	  var reports []models.Report
-	  var names   []string
+	var reports []models.Report
+	var names []string
 
-		err := pc.session.DB("namemanager").C("Person").Find(nil).Distinct("firstName", &names)
-  	if err != nil {
-			w.WriteHeader(404)
-			return
-		}
+	err := pc.session.DB("namemanager").C("Person").Find(nil).Distinct("firstName", &names)
+	if err != nil {
+		w.WriteHeader(404)
+		return
+	}
 
-		for _, name := range names {
-				report := new(models.Report)
-				gender := new(models.Gender)
-				getURLJson("https://gender-api.com/get?key=drdTuwqCXVAMYkReEo&name=" + name, gender)
+	for _, name := range names {
+		report := new(models.Report)
+		gender := new(models.Gender)
+		getURLJson("https://gender-api.com/get?key=drdTuwqCXVAMYkReEo&name="+name, gender)
 
-				count, err := pc.session.DB("namemanager").C("Person").Find(bson.M{"firstName": name}).Count()
-				if err != nil {
-					w.WriteHeader(404)
-					return
-				}
-				report.Instances = count;
-				report.Gender = gender.Gender;
-				report.Accuracy = gender.Accuracy;
-				report.Name = name;
-				reports = append(reports, *report)
-	  }
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
-		if err := json.NewEncoder(w).Encode(reports); err != nil {
-				panic(err)
-		}
-}
-
-// GetPerson gets Person by Id
-func (pc PersonController) GetPerson(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	 	id := p.ByName("id")
-		if !bson.IsObjectIdHex(id) {
-			w.WriteHeader(404)
-			return
-		}
-		oid := bson.ObjectIdHex(id)
-		person := models.Person{}
-		if err := pc.session.DB("namemanager").C("Person").FindId(oid).One(&person); err != nil {
-			w.WriteHeader(404)
-			return
-		}
-		personjson, _ := json.Marshal(person)
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
-		fmt.Fprintf(w, "%s", personjson)
-}
-
-// GetAllPersons gets all persons
-func (pc PersonController) GetAllPersons(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		var persons []models.Person
-		err := pc.session.DB("namemanager").C("Person").Find(nil).All(&persons)
+		count, err := pc.session.DB("namemanager").C("Person").Find(bson.M{"firstName": name}).Count()
 		if err != nil {
 			w.WriteHeader(404)
 			return
 		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
-		if err := json.NewEncoder(w).Encode(persons); err != nil {
-				panic(err)
-		}
+		report.Instances = count
+		report.Gender = gender.Gender
+		report.Accuracy = gender.Accuracy
+		report.Name = name
+		reports = append(reports, *report)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	if err := json.NewEncoder(w).Encode(reports); err != nil {
+		panic(err)
+	}
+}
+
+// GetPerson gets Person by Id
+func (pc PersonController) GetPerson(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(404)
+		return
+	}
+	oid := bson.ObjectIdHex(id)
+	person := models.Person{}
+	if err := pc.session.DB("namemanager").C("Person").FindId(oid).One(&person); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+	personjson, _ := json.Marshal(person)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "%s", personjson)
+}
+
+// GetAllPersons gets all persons
+func (pc PersonController) GetAllPersons(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	var persons []models.Person
+	err := pc.session.DB("namemanager").C("Person").Find(nil).All(&persons)
+	if err != nil {
+		w.WriteHeader(404)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	if err := json.NewEncoder(w).Encode(persons); err != nil {
+		panic(err)
+	}
 }
 
 // CreatePerson creates a new Person resource
 func (pc PersonController) CreatePerson(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		person := models.Person{}
-		json.NewDecoder(r.Body).Decode(&person)
-		person.Id = bson.NewObjectId()
-		pc.session.DB("namemanager").C("Person").Insert(person)
-		personjson, _ := json.Marshal(person)
+	person := models.Person{}
+	json.NewDecoder(r.Body).Decode(&person)
+	person.Id = bson.NewObjectId()
+	pc.session.DB("namemanager").C("Person").Insert(person)
+	personjson, _ := json.Marshal(person)
 
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(201)
-		fmt.Fprintf(w, "%s", personjson)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+	fmt.Fprintf(w, "%s", personjson)
+}
+
+// UpdatePerson updates Person resource
+func (pc PersonController) UpdatePerson(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	id := p.ByName("id")
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(404)
+		return
+	}
+	oid := bson.ObjectIdHex(id)
+
+	person := models.Person{}
+	json.NewDecoder(r.Body).Decode(&person)
+
+	selector := bson.M{"_id": oid}
+	updates := bson.M{"$set": bson.M{"firstName": person.FirstName, "lastName": person.LastName, "ipAddress": person.IpAddress, "country": person.Country}}
+	if err := pc.session.DB("namemanager").C("Person").Update(selector, updates); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	if err := pc.session.DB("namemanager").C("Person").FindId(oid).One(&person); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+	personjson, _ := json.Marshal(person)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "%s", personjson)
 }
 
 // DeletePerson removes an existing Person resource
 func (pc PersonController) DeletePerson(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		id := p.ByName("id")
-		if !bson.IsObjectIdHex(id) {
-			w.WriteHeader(404)
-			return
-		}
-		oid := bson.ObjectIdHex(id)
+	id := p.ByName("id")
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(404)
+		return
+	}
+	oid := bson.ObjectIdHex(id)
 
 	// Delete person
-		if err := pc.session.DB("namemanager").C("Person").RemoveId(oid); err != nil {
-			w.WriteHeader(404)
-			return
-		}
-		w.WriteHeader(200)
+	if err := pc.session.DB("namemanager").C("Person").RemoveId(oid); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+	w.WriteHeader(200)
 }
--- server.go
+++ server.go
@@ -1,32 +1,34 @@
 package main
 
 import (
-    "net/http"
-    "github.com/julienschmidt/httprouter"
-    "github.com/shawnrowley/namemanagergo/controllers"
-    "gopkg.in/mgo.v2"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/shawnrowley/namemanagergo/controllers"
+	"gopkg.in/mgo.v2"
 )
 
 func getSession() *mgo.Session {
-    // Connect to our local mongo
-    s, err := mgo.Dial("mongodb://localhost")
+	// Connect to our local mongo
+	s, err := mgo.Dial("mongodb://localhost")
 
-    if err != nil {
-        panic(err)
-    }
-    return s
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
 
 func main() {
-    r := httprouter.New()
-    pc := controllers.NewPersonController(getSession())
+	r := httprouter.New()
+	pc := controllers.NewPersonController(getSession())
 
-    r.GET("/person", pc.GetAllPersons)
-    r.GET("/person/:id", pc.GetPerson)
-    r.POST("/person", pc.CreatePerson)
-    r.DELETE("/person/:id", pc.DeletePerson)
-    r.GET("/report", pc.GetGenderReport);
+	r.GET("/person", pc.GetAllPersons)
+	r.GET("/person/:id", pc.GetPerson)
+	r.POST("/person", pc.CreatePerson)
+	r.PUT("/person/:id", pc.UpdatePerson)
+	r.DELETE("/person/:id", pc.DeletePerson)
+	r.GET("/report", pc.GetGenderReport)
 
-    // Starts server
-    http.ListenAndServe("localhost:3000", r)
+	// Starts server
+	http.ListenAndServe("localhost:3000", r)
 }
